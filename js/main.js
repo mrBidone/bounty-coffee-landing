f786c99c3e5aefed4e7be97dc32672e98bcebf8b
mrBidone <ab.bz@ukr.net>
2024-09-05 12:50:11 +0200

    Fixed styles

--- js/main.js
+++ js/main.js
@@ -1,16 +1,66 @@
-document
-  .getElementById("google-maps-link")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    toggleSubmenu("google-maps-submenu", "google-arrow");
-  });
+function setUpMenu() {
+  const windowWidth = window.innerWidth;
 
-document
-  .getElementById("yandex-maps-link")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    toggleSubmenu("yandex-maps-submenu", "yandex-arrow");
-  });
+  removeEventListeners();
+
+  if (windowWidth > 768) {
+    const googleLink = document.getElementById("google-maps-link");
+    const yandexLink = document.getElementById("yandex-maps-link");
+
+    googleLink.addEventListener("mouseenter", function () {
+      showSubmenu("google-maps-submenu", "google-arrow");
+    });
+    googleLink.addEventListener("mouseleave", function () {
+      hideSubmenu("google-maps-submenu", "google-arrow");
+    });
+
+    yandexLink.addEventListener("mouseenter", function () {
+      showSubmenu("yandex-maps-submenu", "yandex-arrow");
+    });
+    yandexLink.addEventListener("mouseleave", function () {
+      hideSubmenu("yandex-maps-submenu", "yandex-arrow");
+    });
+  } else {
+    const googleLink = document.getElementById("google-maps-link");
+    const yandexLink = document.getElementById("yandex-maps-link");
+
+    googleLink.addEventListener("click", function (event) {
+      event.preventDefault();
+      toggleSubmenu("google-maps-submenu", "google-arrow");
+    });
+
+    yandexLink.addEventListener("click", function (event) {
+      event.preventDefault();
+      toggleSubmenu("yandex-maps-submenu", "yandex-arrow");
+    });
+  }
+}
+
+function removeEventListeners() {
+  const googleLink = document.getElementById("google-maps-link");
+  const yandexLink = document.getElementById("yandex-maps-link");
+
+  googleLink.replaceWith(googleLink.cloneNode(true));
+  yandexLink.replaceWith(yandexLink.cloneNode(true));
+}
+
+function showSubmenu(submenuId, arrowId) {
+  const submenu = document.getElementById(submenuId);
+  const arrow = document.getElementById(arrowId);
+
+  submenu.style.maxHeight = submenu.scrollHeight + "px";
+  arrow.classList.remove("arrow-right");
+  arrow.classList.add("arrow-down");
+}
+
+function hideSubmenu(submenuId, arrowId) {
+  const submenu = document.getElementById(submenuId);
+  const arrow = document.getElementById(arrowId);
+
+  submenu.style.maxHeight = null;
+  arrow.classList.remove("arrow-down");
+  arrow.classList.add("arrow-right");
+}
 
 function toggleSubmenu(submenuId, arrowId) {
   const submenu = document.getElementById(submenuId);
@@ -26,3 +76,7 @@ function toggleSubmenu(submenuId, arrowId) {
     arrow.classList.add("arrow-down");
   }
 }
+
+window.addEventListener("load", setUpMenu);
+
+window.addEventListener("resize", setUpMenu);
